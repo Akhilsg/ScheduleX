cf41c7840227594f90a50aaa0aa30033827d7920
Akhil Gupta <gupta.akhil076@gmail.com>
2024-09-01 20:17:50 -0700

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { ThemeProvider } from "@mui/material";
+import "./App.css";
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      nigger
+    </ThemeProvider>
+  );
+}
+
+export default App;
